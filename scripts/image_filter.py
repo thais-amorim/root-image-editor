d8039ed5f30f414a9b1da66aed4a434f176a5bac
Thais Amorim <thasmarinho@gmail.com>
2019-09-01 00:29:06 -0300

    Safe convert to uint8 + refactorings

--- scripts/image_filter.py
+++ scripts/image_filter.py
@@ -1,25 +1,26 @@
 import imageio
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 import math
+
+_max_pixel = 255;
 
 def read_image(image_path):
     return imageio.imread(image_path, as_gray=False, pilmode="RGB")
 
 def save_image(path, image_as_byte):
-    imageio.imwrite(path, image_as_byte)
+    imageio.imwrite(path, image_as_byte.astype('uint8'))
 
 def rgb_to_gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 def apply_negative(img_name, img):
-    negative = 255 - img
+    negative = _max_pixel - img
     save_image('images/' + img_name, negative)
 
 def compute_log(img):
-    max_pixel_value = np.max(img)
-    c = (255/np.log(1+max_pixel_value))
+    max_obtained = np.max(img)
+    c = (_max_pixel/np.log(1+max_obtained))
     return  c * np.log(1+img)
 
 def apply_logarithmic(img_name, img):
@@ -28,11 +29,11 @@ def apply_logarithmic(img_name, img):
     save_image('images/' + img_name, log_img)
 
 def apply_gamma_correction(img_name, img, gamma):
-    gamma_correction = ((img/255) ** (1/gamma));
+    gamma_correction = ((img/_max_pixel) ** (1/gamma));
     save_image('images/' + img_name, gamma_correction)
 
 def draw_histogram(img_name, img):
     data = img.flatten()
-    plt.hist(data, 256)
+    plt.hist(data, _max_pixel+1)
     plt.savefig('images/' + img_name) #save_image() doesn't work here. It needs investigation!
     plt.close()
