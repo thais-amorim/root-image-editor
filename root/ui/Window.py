1cd8805c145bb63f2cb85dc3587cc118c7c68967
simao <lucas64_64@hotmail.com>
2019-09-05 23:38:46 -0300

    adicionado menubar e toolbar

--- root/ui/Window.py
+++ root/ui/Window.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QAction, QMainWindow, QApplication, QWidget, QPushButton, QMessageBox
 from PyQt5.QtCore import *
 import sys
 
@@ -8,17 +8,23 @@ class Window(QMainWindow):
         super().__init__()
         self.setGeometry(50,50,800,600)
         self.setWindowTitle("Trabalho de PDI")
-        self.setWindowIcon(QtGui.QIcon('assets/icons/app.ico'))   
-        
-        extractAction = QAction("&Exit", self)
-        extractAction.setShortcut("Ctrl+Q")
-        extractAction.setStatusTip("Leave The App")
-        extractAction.triggered.connect(self.closeApplication)
+        app_icon = QtGui.QIcon()
+        app_icon.addFile('assets/icons/camera-80.png',QSize(80,80))
+        app_icon.addFile('assets/icons/camera-256.png',QSize(256,256))
+        self.setWindowIcon(app_icon)   
+
+        closeAction = QAction("&Exit", self)
+        closeAction.setShortcut("Ctrl+Q")
+        closeAction.setStatusTip("Leave The App")
+        closeAction.triggered.connect(self.closeApplication)
         self.statusBar()
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu("File")
-        fileMenu.addAction(extractAction)
+        editMenu = mainMenu.addMenu("Edit")
+        windowMenu = mainMenu.addMenu("Window")
+        helpMenu = mainMenu.addMenu("Help")
+        fileMenu.addAction(closeAction)
 
         self.home()
 
@@ -27,11 +33,45 @@ class Window(QMainWindow):
         # btn.clicked.connect(self.closeApplication)
         # btn.resize(btn.minimumSizeHint())
         # btn.move(100,100)
+
+        openAction = QAction(QtGui.QIcon('assets/icons/open.png'),"Abrir Ctrl+O", self)
+        saveAction = QAction(QtGui.QIcon('assets/icons/save.png'),"Salvar Ctrl+S", self)
+        saveAllAction = QAction(QtGui.QIcon('assets/icons/save-all.png'),"Salvar Como Ctrl+Shift+S", self)
+        undoAction = QAction(QtGui.QIcon('assets/icons/undo.png'),"Desfazer Ctrl+Z", self)
+        
+        openAction.setShortcut("Ctrl+O")
+        saveAction.setShortcut("Ctrl+S")
+        saveAllAction.setShortcut("Ctrl+Shift+S")
+        undoAction.setShortcut("Ctrl+Z")
+
+        
+        openAction.triggered.connect(self.closeApplication)
+        saveAction.triggered.connect(self.closeApplication)
+        saveAllAction.triggered.connect(self.closeApplication)
+        undoAction.triggered.connect(self.closeApplication)
+
+
+        self.toolbar = self.addToolBar("Extraction")
+        self.toolbar.addAction(openAction)
+        self.toolbar.addAction(saveAction)
+        self.toolbar.addAction(saveAllAction)
+        self.toolbar.addAction(undoAction)
+
+
         self.show()
 
     def closeApplication(self):
-        print("Desligando...")
+        # choice = QtWidgets.QMessagebox.question(self,"Extract!", "Get into the chopper?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        # if choice == QtWidgets.QMessageBox.Yes:
+        print("Desligando....")
         sys.exit()
+        # else:
+        #     pass
+
+
+import ctypes
+myappid = u'mycompany.myproduct.subproduct.version' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 app = QApplication([])
 GUI = Window()
