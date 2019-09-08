b2710766ff69b9d06218d58b87cc06c87b507478
simao <lucas64_64@hotmail.com>
2019-09-08 16:44:27 -0300

    Refatoração da classe de transformada

--- root/controllers/TransformationManager.py
+++ root/controllers/TransformationManager.py
@@ -1,5 +1,6 @@
     
 from ImageManager import ImageManager
+import numpy as np
 
 _max_pixel = 255
 _images_path = 'images/'
@@ -9,19 +10,22 @@ class  TransformationManager(ImageManager):
     def __init__(self):
         super().__init__()
 
-    def apply_negative(self,img_name, img):
+    def apply_negative(self, img):
         negative = _max_pixel - img
-        self.save_image(img_name, negative.astype('uint8'))
+        return negative
+        # self.save_image(img_name, negative)
 
-    def apply_logarithmic(self,img_name, img):
+    def apply_logarithmic(self, img):
         max_obtained = np.max(img)
         c = (_max_pixel/np.log(1+max_obtained))
         log_img = c * np.log(1+img)
-        self.save_image(img_name, log_img.astype('uint8'))
+        return log_img
+        # self.save_image(img_name, log_img.astype('uint8'))
 
-    def apply_gamma_correction(self,img_name, img, gamma):
+    def apply_gamma_correction(self,img, gamma):
         gamma_correction = ((img/_max_pixel) ** (1/gamma))
-        self.save_image(img_name, gamma_correction)
+        return gamma_correction
+        # self.save_image(img_name, gamma_correction)
 
 
 
@@ -29,7 +33,14 @@ f = TransformationManager()
 img = f.read_image("images/einstein.jpg")
 img = f.rgb_to_gray(img)
 #save_image("save_test.jpg", img.copy())
-f.apply_negative("results/negative_test.jpg", img.copy())
+img_negative = f.apply_negative(img)
+img_log = f.apply_logarithmic(img)
+img_gamme = f.apply_gamma_correction( img, 1.5)
+
+f.save_image("results/negative_test.jpg", img_negative)
+f.save_image("results/log_test.jpg", img_log)
+f.save_image("results/gamma_test.jpg", img_gamme)
+
 #apply_logarithmic("log_test.jpg", img.copy())
 #apply_gamma_correction("gamma_test.jpg", img.copy(), 1.5)
 #draw_histogram("histogram_test.jpg", img.copy())
