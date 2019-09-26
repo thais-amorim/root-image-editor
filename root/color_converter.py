b3c2e8e76017334597be1dbedfe9f17ef33596a7
Thais Amorim <thasmarinho@gmail.com>
2019-09-25 22:23:44 -0300

    Convert RGB <-> HSI + tests

--- root/color_converter.py
+++ root/color_converter.py
@@ -1,5 +1,6 @@
 import numpy as np
-import math
+from math import floor, cos, radians, degrees, acos
+
 
 def get_rgb_layers(img):
     r = rgb[:, :, 0]
@@ -8,36 +9,41 @@ def get_rgb_layers(img):
 
     return r, g, b
 
+
 def rgb_to_gray_via_weighted_average(r, g, b):
     return (r * 0.2989) + (g * 0.5870) + (b * 0.1140)
+
 
 def rgb_to_gray_via_simple_average(r, g, b):
     return (r + g + b) / 3
 
-def calculate_hue(r,g,b):
+
+def calculate_hsv_hue(r, g, b):
     max_value = max(r, g, b)
     min_value = min(r, g, b)
-    diff = max_value-min_value
+    diff = max_value - min_value
     if max_value == min_value:
         hue = 0
     elif max_value == r:
-        hue = (60 * ((g-b)/diff) + 360) % 360
+        hue = (60 * ((g - b) / diff) + 360) % 360
     elif max_value == g:
-        hue = (60 * ((b-r)/diff) + 120) % 360
+        hue = (60 * ((b - r) / diff) + 120) % 360
 
     return hue
 
-def calculate_saturation(r,g,b):
+
+def calculate_hsv_saturation(r, g, b):
     max_value = max(r, g, b)
     min_value = min(r, g, b)
-    diff = max_value-min_value
+    diff = max_value - min_value
 
     if max_value == 0:
         saturation = 0
     else:
-        saturation = diff/max_value
+        saturation = diff / max_value
 
     return saturation
+
 
 def rgb_to_hsv(r, g, b):
     '''
@@ -45,14 +51,15 @@ def rgb_to_hsv(r, g, b):
     Input: R, G, B values are [0, 255].
     Output: H value is [0, 360]. S, V values are [0, 1].
     '''
-    r, g, b = r/255.0, g/255.0, b/255.0
+    r, g, b = r / 255.0, g / 255.0, b / 255.0
 
-    h = calculate_hue(r,g,b)
-    s = calculate_saturation(r,g,b)
+    h = calculate_hsv_hue(r, g, b)
+    s = calculate_hsv_saturation(r, g, b)
     v = max(r, g, b)
     return int(h), s, v
 
-def hsv_to_rgb(h,s,v):
+
+def hsv_to_rgb(h, s, v):
     '''
     Convert a HSV image to RGB image.
     Input: H value is [0, 360]. S, V values are [0, 1].
@@ -62,18 +69,103 @@ def hsv_to_rgb(h,s,v):
     s = float(s)
     v = float(v)
     h60 = h / 60.0
-    h60f = math.floor(h60)
+    h60f = floor(h60)
     hi = int(h60f) % 6
     f = h60 - h60f
     p = v * (1 - s)
     q = v * (1 - f * s)
     t = v * (1 - (1 - f) * s)
     r, g, b = 0, 0, 0
-    if hi == 0: r, g, b = v, t, p
-    elif hi == 1: r, g, b = q, v, p
-    elif hi == 2: r, g, b = p, v, t
-    elif hi == 3: r, g, b = p, q, v
-    elif hi == 4: r, g, b = t, p, v
-    elif hi == 5: r, g, b = v, p, q
-    r, g, b = int(r * 255), int(g * 255), int(b * 255)
+    if hi == 0:
+        r, g, b = v, t, p
+    elif hi == 1:
+        r, g, b = q, v, p
+    elif hi == 2:
+        r, g, b = p, v, t
+    elif hi == 3:
+        r, g, b = p, q, v
+    elif hi == 4:
+        r, g, b = t, p, v
+    elif hi == 5:
+        r, g, b = v, p, q
+    return normalize_rgb(r, g, b)
+
+
+def rgb_to_hsi(r, g, b):
+    '''
+    Convert a RGB image to HSI image.
+    Input: R, G, B values are [0, 255].
+    Output: H value is [0, 360]. S, I values are [0, 1].
+    '''
+    r, g, b = normalize_to_zero_one(r, g, b)
+
+    # Hue
+    numerator = 0.5 * ((r - g) + (r - b))
+    denominator = ((r - g) ** 2 + ((r - b) * (g - b))) ** 0.5
+    # A small number was added in the donominator to avoid dividing by 0
+    theta = degrees(acos(numerator / (denominator + 0.000001)))
+    if b <= g:
+        h = theta
+    else:
+        h = 360 - theta
+
+    # Saturation
+    s = 1 - ((3 / (r + g + b)) * min([r, g, b]))
+
+    # Intensity
+    i = (r + g + b) / 3
+
+    return np.round(h, 2), np.round(s, 2), np.round(i, 2)
+
+
+def hsi_to_rgb(h, s, i):
+    '''
+    Convert a HSI image to RGB image.
+    Input: H value is [0, 360]. S, I values are [0, 1].
+    Output: R, G, B values are [0, 255].
+    '''
+    r, g, b = [0, 0, 0]
+
+    # RG sector (0 degrees <= hue < 120 degrees)
+    if 0 <= h < 120:
+        b = i * (1 - s)
+        r = i * (1 + ((s * cos(radians(h))) / (cos(radians(60 - h)))))
+        g = 3 * i - (r + b)
+
+    # GB sector (120 degrees <= hue < 240 degrees)
+    elif 120 <= h < 240:
+        h = h - 120
+        r = i * (1 - s)
+        g = i * (1 + ((s * cos(radians(h))) / (cos(radians(60 - h)))))
+        b = 3 * i - (r + g)
+
+    # BR sector (240 degrees <= hue <= 360 degrees)
+    elif 240 <= h <= 360:
+        h = h - 240
+        g = i * (1 - s)
+        b = i * (1 + ((s * cos(radians(h))) / (cos(radians(60 - h)))))
+        r = 3 * i - (b + g)
+
+    return normalize_rgb(r, g, b)
+
+
+def normalize_to_zero_one(r, g, b):
+    '''
+    Convert RGB values from [0, 255] to [0, 1] range.
+    '''
+    r = float(r) / 255.0
+    g = float(g) / 255.0
+    b = float(b) / 255.0
+
+    return r, g, b
+
+
+def normalize_rgb(r, g, b):
+    '''
+    Convert RGB values from [0, 1] to [0, 255] range.
+    '''
+    r = int(r * 255)
+    g = int(g * 255)
+    b = int(b * 255)
+
     return r, g, b
--- test/test_color_converter.py
+++ test/test_color_converter.py
@@ -1,23 +1,26 @@
 import pytest
 from root import color_converter as converter
+from math import *
+
 
 def test_rgb_to_gray_via_weighted_average():
     r = 200
     g = 133
-    b =  96
+    b = 96
 
     expected = (r * 0.2989) + (g * 0.5870) + (b * 0.1140)
-    obtained = converter.rgb_to_gray_via_weighted_average(r,g,b)
+    obtained = converter.rgb_to_gray_via_weighted_average(r, g, b)
 
     assert obtained == expected
 
+
 def test_rgb_to_gray_via_weighted_average():
     r = 200
     g = 133
-    b =  96
+    b = 96
 
     expected = (r + g + b) / 3
-    obtained = converter.rgb_to_gray_via_simple_average(r,g,b)
+    obtained = converter.rgb_to_gray_via_simple_average(r, g, b)
 
     assert obtained == expected
 
@@ -25,47 +28,249 @@ def test_rgb_to_gray_via_weighted_average():
 def test_rgb_to_hsv_check_h():
     r = 200
     g = 133
-    b =  96
+    b = 96
 
-    h,s,v = converter.rgb_to_hsv(r,g,b)
+    h, s, v = converter.rgb_to_hsv(r, g, b)
     assert h == 21
+
 
 def test_rgb_to_hsv_check_s():
     r = 200
     g = 133
-    b =  96
+    b = 96
 
-    h,s,v = converter.rgb_to_hsv(r,g,b)
+    h, s, v = converter.rgb_to_hsv(r, g, b)
     assert s == 0.52
+
 
 def test_rgb_to_hsv_check_v():
     r = 200
     g = 133
-    b =  96
+    b = 96
 
-    h,s,v = converter.rgb_to_hsv(r,g,b)
+    h, s, v = converter.rgb_to_hsv(r, g, b)
     assert v == 0.7843137254901961
+
 
 def test_hsv_to_rgb_check_r():
     h = 21
     s = 0.52
     v = 0.7843137254901961
 
-    r,g,b = converter.hsv_to_rgb(h,s,v)
+    r, g, b = converter.hsv_to_rgb(h, s, v)
     assert r == 200
+
 
 def test_hsv_to_rgb_check_g():
     h = 21
     s = 0.52
     v = 0.7843137254901961
 
-    r,g,b = converter.hsv_to_rgb(h,s,v)
+    r, g, b = converter.hsv_to_rgb(h, s, v)
     assert g == 132
+
 
 def test_hsv_to_rgb_check_b():
     h = 21
     s = 0.52
     v = 0.7843137254901961
 
-    r,g,b = converter.hsv_to_rgb(h,s,v)
-    assert b ==  96
+    r, g, b = converter.hsv_to_rgb(h, s, v)
+    assert b == 96
+
+
+def test_normalize_to_zero_one_check_interval_r():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_r >= 0
+    assert obtained_r <= 1
+
+
+def test_normalize_to_zero_one_check_r():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_r == 0.12156862745098039
+
+
+def test_normalize_to_zero_one_check_interval_g():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_g >= 0
+    assert obtained_g <= 1
+
+
+def test_normalize_to_zero_one_check_g():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_g == 0.2980392156862745
+
+
+def test_normalize_to_zero_one_check_interval_b():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_b >= 0
+    assert obtained_b <= 1
+
+
+def test_normalize_to_zero_one_check_b():
+    r = 31
+    g = 76
+    b = 122
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_to_zero_one(r, g, b)
+    assert obtained_b == 0.47843137254901963
+
+def test_normalize_rgb_check_interval_r():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_r >= 0
+    assert obtained_r <= 255
+
+
+def test_normalize_rgb_check_r():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_r == 255
+
+
+def test_normalize_rgb_interval_g():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_g >= 0
+    assert obtained_g <= 255
+
+
+def test_normalize_rgb_check_g():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_g == 0
+
+
+def test_normalize_rgb_check_interval_b():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_b >= 0
+    assert obtained_b <= 255
+
+
+def test_normalize_rgb_check_b():
+    r = 1
+    g = 0
+    b = 0.5
+
+    obtained_r, obtained_g, obtained_b = converter.normalize_rgb(r, g, b)
+    assert obtained_b == 127
+
+
+def test_rgb_to_hsi_check_h():
+    r = 31
+    g = 76
+    b = 122
+
+    h, s, i = converter.rgb_to_hsi(r, g, b)
+    assert h == 210.36
+
+
+def test_rgb_to_hsi_check_s():
+    r = 31
+    g = 76
+    b = 122
+
+    h, s, i = converter.rgb_to_hsi(r, g, b)
+    assert s == 0.59
+
+
+def test_rgb_to_hsi_check_i():
+    r = 31
+    g = 76
+    b = 122
+
+    h, s, i = converter.rgb_to_hsi(r, g, b)
+    assert i == 0.30
+
+
+def test_hsi_to_rgb_check_r():
+    h = 210.36
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert r == 31
+
+
+def test_hsi_to_rgb_check_g():
+    h = 210.36
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert g == 76
+
+
+def test_hsi_to_rgb_check_b():
+    h = 210.36
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert b == 121
+
+def test_hsi_to_rgb_rg_sector():
+    h = 80
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert r == 84
+    assert g == 113
+    assert b == 31
+
+def test_hsi_to_rgb_gb_sector():
+    h = 210
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert r == 31
+    assert g == 76
+    assert b == 121
+
+def test_hsi_to_rgb_br_sector():
+    h = 300
+    s = 0.59
+    i = 0.30
+
+    r, g, b = converter.hsi_to_rgb(h, s, i)
+    assert r == 99
+    assert g == 31
+    assert b == 99
