5d7c7fe071871a085c6b0c651fc88c4c9b3a2057
Simão <lucas64_64@hotmail.com>
2019-09-30 14:32:03 -0300

    todas as funções de fft realizadas

--- root/controllers/FourierManager.py
+++ root/controllers/FourierManager.py
@@ -71,6 +71,25 @@ class  FourierManager(ImageManager):
         sF = np.roll(sF,N//2,axis=1)
         return sF
 
+    def ifftshift(self,F):
+        ''' this shifts the centre of FFT of images/2-d signals'''
+        
+        M, N = F.shape
+        # med_m = M//2 + M%2
+        # med_n = N//2 + N%2
+
+        
+        # R1, R2 = F[0: M//2, 0: N//2], F[M//2: M, 0: N//2]
+        # R3, R4 = F[0: M//2, N//2: N], F[M//2: M, N//2: N]
+
+        # sF = np.zeros(F.shape,dtype = F.dtype)
+        # sF[med_m: M, med_n: N], sF[0: med_m, 0: med_n] = R1, R4
+        # sF[med_m: M, 0: med_n], sF[0: med_m, med_n: N]= R3, R2
+        sF = np.roll(F,-(N//2),axis=1)
+        sF = np.roll(sF,-(M//2),axis=0)
+
+        return sF
+
 
     def ifft(self,fu):
         """ use recursive method to speed up"""
@@ -113,35 +132,11 @@ img = np.around( f.rgb_to_gray(img))
 
 
 
-# x = [0,1,2,3,4,5,6,7,8,9,10]
-x = np.random.rand(5,10)
-# print(x)
-# print(x[0::2][0::2][0::2][0::2])
-# print(x[1::2])
-# print(x[0:2])
 
-# n = x.shape[0]
-# e = math.ceil(abs(cmath.log(n,2)))
-# print(e)
-# print(2**e)
-# v = np.zeros(2**e)
-# v[:x.shape[0]] = x
-# print(v)
-
-
-# obt = f.fft(x)
-# expected = np.fft.fft(x)
-# print(expected)
-# print("------")
-# print(obt)
-
-# print(np.allclose(obt,expected))
 
 x = img
 obt = f.fft2(x)
 expected = np.fft.fft2(x)
-
-
 
 # print(expected)
 # print("------")
@@ -167,54 +162,33 @@ print(np.allclose(shift,shift2))
 
 
 
+ishift = f.ifftshift(shift2)
+print('Test ishift')
+print(np.allclose(obt,ishift))
+
+
 
 y = np.random.rand(10)
 y = f.fft(y)
 obt2 = f.ifft(y)
 expected2 = np.fft.ifft(y)
 
-print(obt2)
-print("------")
-print(expected2)
+# print(obt2)
+# print("------")
+# print(expected2)
 
 print("Teste ifft")
 print(np.allclose(obt2,expected2))
 
 
-obt = f.ifft2(obt)
-expected = np.fft.ifft2(expected)
-print("Teste ifft")
-print(np.allclose(obt,expected))
+obt = f.ifftshift(shift)
+
+obt2 = f.ifft2(obt)
+expected = np.fft.ifft2(obt)
+print("Teste ifft2")
+print(np.allclose(obt2,expected))
 
 import matplotlib.pyplot as plt
-plt.imshow(obt)
+plt.imshow(obt2,cmap='gray')
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-# h,w = img.shape
-# for i in range (h):
-#     print(np.allclose(f.fft(img[i,:]), np.fft.fft(img[i,:])))
-    
-
-# x = np.random.random(1024)
-# f  = FourierManager()
-# print(np.allclose(f.fft(x), np.fft.fft(x)))
-
-# img = f.read_image("./images/photography.jpg")
-# img = f.rgb_to_gray(img)
-# print(np.fft.fft2(img))
-# print('---------------------------------------')
-# print('---------------------------------------')
-# print('---------------------------------------')
-# print('---------------------------------------')
-# print(f.fft2(img))
 
