b1d6b9c03323078bf192d223b6118024f204ff10
Simão <lucas64_64@hotmail.com>
2019-10-13 22:40:09 -0300

    Adaptado o TransformController para receber as funções criadas pela Thaís

--- root/controllers/TransformationController.py
+++ root/controllers/TransformationController.py
@@ -1,13 +1,17 @@
 
 from TransformationManager import TransformationManager
 import numpy as np
+
+from filter import ImageFilter as filter
+from filter import ColorFilter as color
+from converter import ColorConverter as converter
+
+
 class TransformationController():
 
     def __init__(self):
         super().__init__()
         self.original_image = self.current_image = self.undo_image = None
-        
-        self.transform = TransformationManager()
     
     def getCurrentImage(self):
         return self.current_image
@@ -17,25 +21,25 @@ class TransformationController():
         return self.current_image
 
     def loadImage(self,image):
-        img = self.transform.read_image(image)
-        self.original_image = self.transform.rgb_to_gray(img)
+        img = filter.read_image(image)
+        self.original_image = converter.rgb_to_gray(img)
         self.current_image = self.original_image
         self.undo_image = self.original_image
         return self.current_image
 
     def negativeTransform(self):
         self.undo_image = self.current_image
-        self.current_image  = (self.transform.apply_negative(self.current_image)).astype(np.uint8)
+        self.current_image  = (filter.apply_negative(self.current_image)).astype(np.uint8)
         return self.current_image
 
     def logarithmicTransform(self):
         self.undo_image = self.current_image
-        self.current_image  = self.transform.apply_logarithmic(self.current_image)
+        self.current_image  = filter.apply_logarithmic(self.current_image)
         return self.current_image
 
     def gammaTransform(self,gamma):
         self.undo_image = self.current_image
-        self.current_image  = self.transform.apply_gamma_correction(self.current_image, gamma)
+        self.current_image  = filter.apply_gamma_correction(self.current_image, gamma)
         return self.current_image
 
     def apply_equalized_histogram(self,img_name, img):
